c7962a794ea3e2076a2c10a386a13634af44f324
shumaita <123713655+Shumaita@users.noreply.github.com>
2024-06-25 02:27:50 +0600

    Add files via upload

---
+++ FIFO(First_In_First_Out).cpp
@@ -0,0 +1,96 @@
+// First In First Out algorithm
+// C++ implementation of FIFO page replacement with the help of chatgpt
+// in Operating Systems.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Function to find page faults using FIFO
+int pageFaults(int pages[], int l, int capacity)
+{
+    // To represent set of current pages. We use
+    // an unordered_set so that we quickly check
+    // if a page is present in set or not
+    unordered_set<int> s;
+
+    // To store the pages in FIFO manner
+    queue<int> indexes;
+
+    // Start from initial page
+    int page_faults = 0;
+    for (int i = 0; i < l; i++) // Loop Complexity: O(l)
+    {
+        // Check if the set can hold more pages
+        if (s.size() < capacity)
+        {
+            // Insert it into set if not present
+            // already which represents page fault
+            if (s.find(pages[i]) == s.end())
+            {
+                // Insert the current page into the set
+                s.insert(pages[i]);
+
+                // Increment page fault
+                page_faults++;
+
+                // Push the current page into the queue
+                indexes.push(pages[i]);
+            }
+        }
+
+        // If the set is full then need to perform FIFO
+        // i.e. remove the first page of the queue from
+        // set and queue both and insert the current page
+        else
+        {
+            // Check if current page is not already
+            // present in the set
+            if (s.find(pages[i]) == s.end())
+            {
+                // Store the first page in the
+                // queue to be used to find and
+                // erase the page from the set
+                int val = indexes.front();
+
+                // Pop the first page from the queue
+                indexes.pop();
+
+                // Remove the indexes page from the set
+                s.erase(val);
+
+                // insert the current page in the set
+                s.insert(pages[i]);
+
+                // push the current page into
+                // the queue
+                indexes.push(pages[i]);
+
+                // Increment page faults
+                page_faults++;
+            }
+        }
+    }
+
+    return page_faults;
+}
+
+int main()
+{
+    int l;
+    cout << "Enter the number of pages: ";//total page can insert:20
+    cin >> l;//20
+    int capacity;
+    cout << "Enter the number of frames: ";
+    cin >> capacity;//3
+    int pages[l];
+    cout << "Enter the page reference string: ";
+    //7 0 1 2 0 3 0 4 2 3 0 3 2 1 2 0 1 7 0 1
+    for (int i = 0; i < l; ++i) {
+        cin >> pages[i];
+    }
+    // Call the pageFaults function and print the result
+    cout<<"Total page faults:"<<endl;
+    cout << pageFaults(pages, l, capacity) << endl;
+    return 0;
+}
+
